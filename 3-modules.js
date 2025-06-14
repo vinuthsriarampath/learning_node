bb0010fafd8cc4a142b31ebee4f8243e9d9314b0
vinuth <vinuthsriarampath@outlook.com>
2025-06-14 12:02:15 +0530

    Add: Integrate module imports in 3-modules.js for names, utilities, and alternative data

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+// CommonJs, every file is module (by default)
+// Modules -  Encapsulated Code (only share minimum)
+
+const names =  require('./4-name');
+const sayHi = require('./5-utils');
+const  data = require('./6-alternative-flavor');
+require('./7-mind-grenade')
+// sayHi("Susan");
+// sayHi(names.john);
+// sayHi(names.peter);
